59e8ee4bfdb15d29c1727f2d7dba73e378d42bec
Derick96 <60660484+Derick96@users.noreply.github.com>
2020-03-13 17:47:33 -0600

    primer avance

--- src/edu/ujcv/progra1/HeapSort.java
+++ src/edu/ujcv/progra1/HeapSort.java
@@ -1,29 +1,66 @@
-package edu.ujcv.progra1;
 
-import java.util.PriorityQueue;
+import edu.ujcv.progra1.SortTester;
 
-public class HeapSort implements SortTester {
-    @Override
-    public long sort(int[] array) {
-        long start = System.currentTimeMillis();
-        heapSort(array);
+import java.util.Arrays;
+import java.util.Scanner;
+public abstract class HeapSort implements SortTester {
+    private static Scanner sc;
 
-        long end = System.currentTimeMillis();
+    public static void main(String args[]) {
 
-        return end - start;
+        sc = new Scanner(System.in);
+
+        System.out.println("Ingrese la cantidad de elementos que desea ordenar");
+        int n = sc.nextInt();
+
+
+        System.out.println("Ingrese los numeros a ordenar: ");
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+
+        System.out.println("El arreglo sin ordenar es: ");
+        System.out.println(Arrays.toString(arr));
+
+        heap(arr);
+
+        System.out.println("El arreglo ordenado es: ");
+        System.out.println(Arrays.toString(arr));
     }
 
-    public static int[] heapSort(int[] array) {
+    static void heapify(int a[], int n, int i) {
+        int max, child;
+        child = 2 * i + 1;
+        max = i;
+        if (child < n)
+            if (a[child] > a[max])
+                max = child;
+        if (child + 1 < n)
+            if (a[child + 1] > a[max])
+                max = child + 1;
+        if (max != i) {
+            int temp = a[i];
+            a[i] = a[max];
+            a[max] = temp;
+            heapify(a, n, max);
+        }
+    }
 
-        // cambiar priority queue por su propia implementacion!!
-        PriorityQueue <Integer> queue = new PriorityQueue<>();
-        for (Integer i: array) {
-            queue.add(i);
+    static void buildheap(int a[]) {
+        for (int i = a.length / 2 - 1; i >= 0; i--)
+            heapify(a, a.length, i);
+    }
+
+    static void heap(int a[]) {
+        buildheap(a);
+        for (int i = a.length - 1; i >= 1; i--) {
+            int temp = a[0];
+            a[0] = a[i];
+            a[i] = temp;
+            heapify(a, i, 0);
         }
-        for (int i = 0; i < array.length; i++) {
-            array[i] = queue.remove();
-        }
-        return array;
     }
 
 }
+
+
