0ce42b2b41ae39e6e1a3efba09fbe64a9da73e2d
Derick96 <60660484+Derick96@users.noreply.github.com>
2020-03-16 10:10:36 -0600

    Iniciando codigo MergeSort

--- src/edu/ujcv/progra1/MergeSort.java
+++ src/edu/ujcv/progra1/MergeSort.java
@@ -3,16 +3,30 @@ package edu.ujcv.progra1;
 public class MergeSort  implements SortTester{
     @Override
     public long sort(int[] array) {
-        long start = System.currentTimeMillis();
+        long start = System.nanoTime();
         mergeSort(array);
 
-        long end = System.currentTimeMillis();
+        long end = System.nanoTime();
 
         return end - start;
     }
 
-    public static int[] mergeSort(int[] a){
-        return new int[5];
+    public static int[] mergeSort(int[] arr){
+        if (arr.length <= 1) {
+            return arr;
+        }
+        int medio = arr.length / 2;
+
+        int[] abajo = new int[medio];
+        int[] arriba = new int[arr.length - medio];
+        for (int a = 0; a < medio; a++) {
+            abajo[a] = arr[a];
+        }
+
+        for (int a = 0; a < arriba.length; a++) {
+            arriba[a] = arr[a + abajo.length];
+        }
+        return merge(mergeSort(abajo), mergeSort(arriba));
     }
 
     public static int[] merge(int[] inf, int[] sup ){
